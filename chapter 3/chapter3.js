376baaa7bb34c0490f80e12f8843f9c4ac420835
KeikantsengMokgophe <keikantsengmogomotsi7@gmail.com>
2026-04-29 12:53:30 +0200

    Add files via upload
    chapter 3

---
+++ chapter 3/chapter3.js
@@ -0,0 +1,8 @@
+var toName = "kantse";
+var fromName = "the grammy awards";
+var letterBody =
+    "we are pleased to inform you that your song, 'can\'t stop coding!,' has been voted the best song of all times by the awarding committee"
+document.write("dear " + toName + ",<br><br>")
+document.write(letterBody + "<br><br>");
+document.write("sincerely,<br>");
+document.write(fromName);
